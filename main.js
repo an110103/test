1465bba121751ae5f1dd4ccdf22a44ee20bd2c0a
DNA <an.duong110103@hcmut.edu.vn>
2023-09-22 10:53:47 +0700

    new

---
+++ main.js
@@ -0,0 +1,18 @@
+var myArray = [
+    'hello',
+    'hi',
+    'chào'
+]
+
+var myInfo = {
+    name: 'Ân',
+    age: 20,
+    address: 'TPHCM'
+}
+
+for( var key of Object.keys(myInfo)){
+    console.log(myInfo[key])
+}
+
+console.log(200)
+
